5c34d44ec3179263734a24aa65ee7c8324ca15b5
James Leone <jleone@umass.edu>
2024-10-08 02:40:23 -0400

    Adding script and profile picture, which I accidentally removed - these are in the previous commit

---
+++ getHome.js
@@ -0,0 +1,11 @@
+// let homeTag = document.getElementById("home-link");
+// let mainTag = document.getElementById("main");
+// console.log(homeTag);
+
+// homeTag.addEventListener("click", (e) => {
+//   let h1 = document.createElement("h1");
+//   h1.innerText = "Hi!";
+//   mainTag.innerHTML = "";
+//   mainTag.appendChild(h1);
+//   return;
+// });
